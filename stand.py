2b9899133979dfd6ba96465941b506f0e0caaa51
theycantstopusall <jordantownsend2014@gmail.com>
2022-02-17 18:25:48 +0000

    Intial

---
+++ stand.py
@@ -0,0 +1,13 @@
+from operator import indexOf
+import random
+import aiofiles
+
+def pick():
+    icao = input("What ICAO are you controlling?")
+    
+        
+    else:
+        print("sorry no can do")
+
+
+pick()

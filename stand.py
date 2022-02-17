76bb22450c5bb4f915011e849d49add18e12d696
theycantstopusall <jordantownsend2014@gmail.com>
2022-02-17 18:36:56 +0000

    Intial ideas

--- stand.py
+++ stand.py
@@ -4,8 +4,15 @@ import aiofiles
 
 def pick():
     icao = input("What ICAO are you controlling?")
-    
-        
+    if icao == "egss":
+        airline = input("What is the airlines ICAO? I.e RYR?")
+        if airline == "RYR":
+            standr = random.choice(range(10,150))
+            print(standr)
+        else:
+            if airline == "EZY":
+                stand = random.choice(range(10,50))
+                print(stand)
     else:
         print("sorry no can do")
 
